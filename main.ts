dc763bf6d42e730b2b388cc8b1537fe5d27a5db0
mix-big <mixbig3343@gmail.com>
2025-02-23 21:14:43 +0900

    yyeu

--- main.ts
+++ main.ts
@@ -8,33 +8,42 @@ function 地震検出 () {
         `)
     basic.showLeds(`
         . . . . .
+        . # . # .
+        # . . . #
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
         . . . # .
         # . # . #
         . # . . .
         . . . . .
         `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        # . . . #
+        . # . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
 }
 input.onGesture(Gesture.Shake, function () {
-    地震検出()
-    while (!(PGA.getPGA() == 0)) {
-        radio.sendString(PGA.toJMASeismicIntensity(PGA.getPGA()))
-        震度表示()
-        basic.showNumber(PGA.toLongPeriodSeismicIntensity(PGA.getPGA()))
-        if (PGA.isJMASeismicAbove(PGA.getPGA(), "5-")) {
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                . . # . .
-                . . . . .
-                . . # . .
-                `)
-            music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        }
-    }
-})
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-    music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+	
 })
 function 震度表示 () {
     if (250 <= PGA.getPGA() && 400 > PGA.getPGA()) {
@@ -73,3 +82,6 @@ function 震度表示 () {
         basic.showString(PGA.toJMASeismicIntensity(PGA.getPGA()))
     }
 }
+basic.forever(function () {
+    震度表示()
+})
